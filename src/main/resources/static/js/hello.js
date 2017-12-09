76e2fc1239b4b30db339757270571696ed53f9f1
HelmutSteiner <helmut.steinersti2@gmail.com>
2017-12-09 11:02:19 +0100

    Added index.html and js folder.

---
+++ src/main/resources/static/js/hello.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+var AppComponent = ng.core.Component({
+    selector : 'app',
+    template : '<p>The ID is {{greeting.id}}</p><p>The content is {{greeting.content}}</p>{{farmhand.name}}'
+}).Class({
+    constructor : function() {
+        this.greeting = {id:'XYZ', content:'Hello World'};
+        this.farmhand = {name: 'FarmHand'};
+    }
+});
+
+var AppModule = ng.core.NgModule({
+    imports: [ng.platformBrowser.BrowserModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+  }).Class({constructor : function(){}})
+
+document.addEventListener('DOMContentLoaded', function() {
+    ng.platformBrowserDynamic.platformBrowserDynamic().bootstrapModule(AppModule);
+});
