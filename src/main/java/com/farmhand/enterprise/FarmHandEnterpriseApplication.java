0532e429401438669f1a5c037fdef1ec2cc8e6ef
HelmutSteiner <helmut.steinersti2@gmail.com>
2017-12-22 12:41:20 +0100

    Added whole infrastructure code for data source plus maven depend.

--- src/main/java/com/farmhand/enterprise/FarmHandEnterpriseApplication.java
+++ src/main/java/com/farmhand/enterprise/FarmHandEnterpriseApplication.java
@@ -2,8 +2,10 @@ package com.farmhand.enterprise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class FarmHandEnterpriseApplication {
 
 	public static void main(String[] args) {
