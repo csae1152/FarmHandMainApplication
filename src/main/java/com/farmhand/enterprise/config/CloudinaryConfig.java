d8e29127f74dbe60805c0dddda831698a9d99eee
HelmutSteiner <helmut.steinersti2@gmail.com>
2018-07-17 12:56:16 +0200

    Adds cloudinary config.

---
+++ src/main/java/com/farmhand/enterprise/config/CloudinaryConfig.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.farmhand.enterprise.config;
+
+import com.cloudinary.Cloudinary;
+
+/**
+ * @author Helmut
+ *
+ */
+public class CloudinaryConfig {
+	Cloudinary cloudinary = new Cloudinary();
+	
+	private void setUpConfig() {
+		
+	}
+}
